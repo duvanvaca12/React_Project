92b90317806349613c69eb3da61062954970a364
Cesar <=>
2022-05-30 02:18:19 +1000

    Fixed menu link

--- src/Pages/HommePage.js
+++ src/Pages/HommePage.js
@@ -1,37 +1,9 @@
 import * as React from "react";
-import Box from "@mui/material/Box";
-import Drawer from "@mui/material/Drawer";
-import Button from "@mui/material/Button";
-import List from "@mui/material/List";
-import Divider from "@mui/material/Divider";
-import ListItem from "@mui/material/ListItem";
-import ListItemButton from "@mui/material/ListItemButton";
-import ListItemIcon from "@mui/material/ListItemIcon";
-import ListItemText from "@mui/material/ListItemText";
-import Login from "@mui/icons-material/AccountCircle";
-import HomeIcon from "@mui/icons-material/Home";
-import VolcanoIcon from "@mui/icons-material/Volcano";
 import Typography from "@mui/material/Typography";
 import hcbgImage from "/home/cesar/Documents/Proyect_React/volcano-data/src/Resources/background.jpg";
 import "@fontsource/roboto/700.css";
-import Menu from "../components/menu";
 
 export default function HomePage() {
-  const [state, setState] = React.useState({
-    menu: false,
-  });
-
-  const toggleDrawer = (anchor, open) => (event) => {
-    if (
-      event.type === "keydown" &&
-      (event.key === "Tab" || event.key === "Shift")
-    ) {
-      return;
-    }
-
-    setState({ ...state, [anchor]: open });
-  };
-
   return (
     <div
       class="bg_image"
@@ -48,7 +20,6 @@ export default function HomePage() {
         Search Info About volcanos <br />
         around the world
       </Typography>
-      {Menu()}
     </div>
   );
 }
